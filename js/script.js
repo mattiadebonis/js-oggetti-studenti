9093e15b6a5be2a6612cd7766c03587ef3d62f70
mattiadebonis <mattia.debonis@gmail.com>
2021-05-10 15:57:35 +0200

    Creo oggetto studente e stampo le proprietà

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+var studente = {
+    'nome': "Mattia",
+    'studente': "De Bonis",
+    'età': 33
+};
+
+
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+for(indice in studente){
+    console.log(studente[indice].nome);
+
+}
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all'utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell'ordine: nome, cognome e età.
+
+
+
